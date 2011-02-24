203e4e752d46439481360d6323d441bb98894733
Kaspar Schiess <kaspar.schiess@absurd.li>
2011-02-24 11:54:17 +0100

    - removes comments that would appear nonsensical in sdoc
    These comments would appear as documentation for the Net module, which is bogus. _and_ the formatting was off. Removing them makes rdoc appear more clear.

--- lib/net/ber.rb
+++ lib/net/ber.rb
@@ -1,27 +1,3 @@
-# NET::BER
-# Mixes ASN.1/BER convenience methods into several standard classes. Also
-# provides BER parsing functionality.
-#
-#--
-# Copyright (C) 2006 by Francis Cianfrocca. All Rights Reserved.
-#
-# Gmail: garbagecat10
-#
-# This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the Free
-# Software Foundation; either version 2 of the License, or (at your option)
-# any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-# for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc., 51
-# Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#++
-
 module Net
   ##
   # == Basic Encoding Rules (BER) Support Module
