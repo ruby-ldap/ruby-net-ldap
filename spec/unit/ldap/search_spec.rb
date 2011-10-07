2a74577d5f08b1b2a4ac33b8d9753b3f8d1efba4
Daniel Abrahamsson <hamsson@gmail.com>
2011-10-07 15:56:55 +0200

    search should return result set if :return_result is unspecified (nil).
    Corrects incorrect behaviour introduced in a4819e525f29d83357a298065df06acf5903b1af

--- spec/unit/ldap/search_spec.rb
+++ spec/unit/ldap/search_spec.rb
@@ -13,17 +13,24 @@ describe Net::LDAP, "search method" do
     @connection.instance_variable_set(:@open_connection, FakeConnection.new)
   end
 
-  context "when returning result set" do
+  context "when :return_result => true" do
     it "should return nil upon error" do
       result_set = @connection.search(:return_result => true)
       result_set.should be_nil
     end
   end
 
-  context "when returning boolean" do
+  context "when :return_result => false" do
     it "should return false upon error" do
       success = @connection.search(:return_result => false)
       success.should == false
+    end
+  end
+
+  context "When :return_result is not given" do
+    it "should return nil upon error" do
+      result_set = @connection.search
+      result_set.should be_nil
     end
   end
 end
