656039cee0708bb7cb5673eade7a821df03b5d9a
Harold A. Jones II <github-commits@cfxmusic.com>
2012-04-30 22:03:55 -0400

    Gracefully fall back if string cannot be encoded.

--- lib/net/ber/core_ext/string.rb
+++ lib/net/ber/core_ext/string.rb
@@ -20,7 +20,11 @@ module Net::BER::Extensions::String
     if self.respond_to?(:encode)
       # Strings should be UTF-8 encoded according to LDAP.
       # However, the BER code is not necessarily valid UTF-8
-      self.encode('UTF-8').force_encoding('ASCII-8BIT')
+      begin
+        self.encode('UTF-8').force_encoding('ASCII-8BIT')
+      rescue Encoding::UndefinedConversionError
+        self
+      end
     else
       self
     end
