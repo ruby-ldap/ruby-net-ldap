09b054264181225fdfd7f28095bb6c5272990a1e
Mattias Ohlsson <mattias.ohlsson@inprose.com>
2014-07-01 09:58:55 +0200

    handle base64 encoded dn attr

--- lib/net/ldap/dataset.rb
+++ lib/net/ldap/dataset.rb
@@ -125,8 +125,10 @@ class Net::LDAP::Dataset < Hash
           if line =~ /^#/
             ds.comments << line
             yield :comment, line if block_given?
-          elsif line =~ /^dn:[\s]*/i
-            dn = $'
+          elsif line =~ /^dn:([\:]?)[\s]*/i
+            # $1 is a colon if the dn-value is base-64 encoded
+            # $' is the dn-value
+            dn = ($1 == ":") ? $'.unpack('m').shift : $'
             ds[dn] = Hash.new { |k,v| k[v] = [] }
             yield :dn, dn if block_given?
           elsif line.empty?
