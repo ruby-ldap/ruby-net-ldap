903e459e7bde5b0ce03c839a8fcbf7a570a9eaf7
Jerry Cheung <jch@whatcodecraves.com>
2014-10-10 14:50:42 -0700

    fix deprecation any_instances

--- test/ber/core_ext/test_string.rb
+++ test/ber/core_ext/test_string.rb
@@ -17,7 +17,7 @@ class TestBERStringExtension < Test::Unit::TestCase
   def test_exception_does_not_modify_string
     original = "0$\002\001\001`\037\002\001\003\004\rAdministrator\200\vad_is_bogus".b
     duplicate = original.dup
-    flexmock(StringIO).any_instance.should_receive(:read_ber).and_raise(Net::BER::BerError)
+    flexmock(StringIO).new_instances.should_receive(:read_ber).and_raise(Net::BER::BerError)
     duplicate.read_ber!(Net::LDAP::AsnSyntax) rescue Net::BER::BerError
 
     assert_equal original, duplicate
