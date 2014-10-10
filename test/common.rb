b2d49b945b5fafcc83febbf6c27eb0f0c11b2d79
Jerry Cheung <jch@whatcodecraves.com>
2014-10-10 12:58:39 -0700

    add MockInstrumentationService to common

--- test/common.rb
+++ test/common.rb
@@ -2,3 +2,21 @@
 require 'test/unit'
 require 'net/ldap'
 require 'flexmock/test_unit'
+
+class MockInstrumentationService
+  def initialize
+    @events = {}
+  end
+
+  def instrument(event, payload)
+    result = yield(payload)
+    @events[event] ||= []
+    @events[event] << [payload, result]
+    result
+  end
+
+  def subscribe(event)
+    @events[event] ||= []
+    @events[event]
+  end
+end
