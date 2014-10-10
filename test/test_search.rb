231bd873506c50a68e1b14fb714dbf15c387d4c6
Jerry Cheung <jch@whatcodecraves.com>
2014-10-10 12:58:47 -0700

    convert to test/unit

--- test/test_search.rb
+++ test/test_search.rb
@@ -1,49 +1,39 @@
 # -*- ruby encoding: utf-8 -*-
+require 'common'
 
-describe Net::LDAP, "search method" do
+class TestSearch < Test::Unit::TestCase
   class FakeConnection
     def search(args)
       OpenStruct.new(:result_code => 1, :message => "error", :success? => false)
     end
   end
 
-  before(:each) do
+  def setup
     @service = MockInstrumentationService.new
     @connection = Net::LDAP.new :instrumentation_service => @service
     @connection.instance_variable_set(:@open_connection, FakeConnection.new)
   end
 
-  context "when :return_result => true" do
-    it "should return nil upon error" do
-      result_set = @connection.search(:return_result => true)
-      result_set.should be_nil
-    end
+  def test_true_result
+    assert_nil @connection.search(:return_result => true)
   end
 
-  context "when :return_result => false" do
-    it "should return false upon error" do
-      result = @connection.search(:return_result => false)
-      result.should == false
-    end
+  def test_false_result
+    refute @connection.search(:return_result => false)
   end
 
-  context "When :return_result is not given" do
-    it "should return nil upon error" do
-      result_set = @connection.search
-      result_set.should be_nil
-    end
+  def test_no_result
+    assert_nil @connection.search
   end
 
-  context "when instrumentation_service is configured" do
-    it "should publish a search.net_ldap event" do
-      events = @service.subscribe "search.net_ldap"
+  def test_instrumentation_publishes_event
+    events = @service.subscribe "search.net_ldap"
 
-      @connection.search :filter => "test"
+    @connection.search(:filter => "test")
 
-      payload, result = events.pop
-      payload.should have_key(:result)
-      payload.should have_key(:filter)
-      payload[:filter].should == "test"
-    end
+    payload, result = events.pop
+    assert payload.has_key?(:result)
+    assert payload.has_key?(:filter)
+    assert_equal "test", payload[:filter]
   end
 end
