26ebfff2ef5f73d6885676a5aa5e18c6167a6532
Matt Todd <chiology@gmail.com>
2014-10-14 18:07:06 -0700

    Override Test::Unit::TestCase#run for disabled integration tests

--- test/test_integration.rb
+++ test/test_integration.rb
@@ -1,38 +1,42 @@
 require_relative 'test_helper'
 
-if !INTEGRATION
-  puts "Skipping integration tests..."
-else
-  class TestLDAPIntegration < Test::Unit::TestCase
-    def setup
-      @service = MockInstrumentationService.new
-      @ldap = Net::LDAP.new \
-        host:           'localhost',
-        port:           389,
-        admin_user:     'uid=admin,dc=rubyldap,dc=com',
-        admin_password: 'passworD1',
-        search_domains: %w(dc=rubyldap,dc=com),
-        uid:            'uid',
-        instrumentation_service: @service
-    end
+class TestLDAPIntegration < Test::Unit::TestCase
 
-    def test_bind_success
-      assert @ldap.bind(method: :simple, username: "uid=user1,ou=People,dc=rubyldap,dc=com", password: "passworD1"), @ldap.get_operation_result.inspect
+  # If integration tests aren't enabled, noop these tests.
+  if !INTEGRATION
+    def run(*)
+      self
     end
+  end
 
-    def test_bind_anonymous_fail
-      refute @ldap.bind(method: :simple, username: "uid=user1,ou=People,dc=rubyldap,dc=com", password: ""), @ldap.get_operation_result.inspect
+  def setup
+    @service = MockInstrumentationService.new
+    @ldap = Net::LDAP.new \
+      host:           'localhost',
+      port:           389,
+      admin_user:     'uid=admin,dc=rubyldap,dc=com',
+      admin_password: 'passworD1',
+      search_domains: %w(dc=rubyldap,dc=com),
+      uid:            'uid',
+      instrumentation_service: @service
+  end
 
-      result = @ldap.get_operation_result
-      assert_equal 53, result.code
-      assert_equal "Unwilling to perform", result.message
-      assert_equal "unauthenticated bind (DN with no password) disallowed",
-        result.error_message
-      assert_equal "", result.matched_dn
-    end
+  def test_bind_success
+    assert @ldap.bind(method: :simple, username: "uid=user1,ou=People,dc=rubyldap,dc=com", password: "passworD1"), @ldap.get_operation_result.inspect
+  end
 
-    def test_bind_fail
-      refute @ldap.bind(method: :simple, username: "uid=user1,ou=People,dc=rubyldap,dc=com", password: "not my password"), @ldap.get_operation_result.inspect
-    end
+  def test_bind_anonymous_fail
+    refute @ldap.bind(method: :simple, username: "uid=user1,ou=People,dc=rubyldap,dc=com", password: ""), @ldap.get_operation_result.inspect
+
+    result = @ldap.get_operation_result
+    assert_equal 53, result.code
+    assert_equal "Unwilling to perform", result.message
+    assert_equal "unauthenticated bind (DN with no password) disallowed",
+      result.error_message
+    assert_equal "", result.matched_dn
+  end
+
+  def test_bind_fail
+    refute @ldap.bind(method: :simple, username: "uid=user1,ou=People,dc=rubyldap,dc=com", password: "not my password"), @ldap.get_operation_result.inspect
   end
 end
