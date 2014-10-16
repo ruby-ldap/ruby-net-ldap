f16a4ff609b0f6dc4ec4abe04e3d9adeed745708
Matt Todd <chiology@gmail.com>
2014-10-15 22:27:50 -0700

    Add failing test for Net::LDAP#open with nested queries
    The nested query should match "user2", but with Net::LDAP#open, we see:
    
      <"user3"> expected but was <"user2">.
    
    And likewise with the outer search:
    
      <["user1", "user2"]> expected but was <["user1", "user3"]>.
    
    What's happening is that the nested query is being sent after the server
    has already sent more data to be read for the outer search, so when the
    inner search reads, it gets that result. And when it comes time for the
    outer search to handle the second results, it gets reads the next result
    off the wire, the result of the inner search.
    
    I was also able to get an infinite loop just by *always* performing an
    inner search instead of only searching on the first result.

--- test/integration/test_open.rb
+++ test/integration/test_open.rb
@@ -20,4 +20,32 @@ class TestBindIntegration < LDAPIntegrationTestCase
 
     assert_equal 1, events.size
   end
+
+  def test_nested_search_without_open
+    entries = []
+    nested_entry = nil
+
+    @ldap.search(filter: "(|(uid=user1)(uid=user2))", base: "ou=People,dc=rubyldap,dc=com") do |entry|
+      entries << entry.uid.first
+      nested_entry ||= @ldap.search(filter: "uid=user3", base: "ou=People,dc=rubyldap,dc=com").first
+    end
+
+    assert_equal "user3", nested_entry.uid.first
+    assert_equal %w(user1 user2), entries
+  end
+
+  def test_nested_search_with_open
+    entries = []
+    nested_entry = nil
+
+    @ldap.open do
+      @ldap.search(filter: "(|(uid=user1)(uid=user2))", base: "ou=People,dc=rubyldap,dc=com") do |entry|
+        entries << entry.uid.first
+        nested_entry ||= @ldap.search(filter: "uid=user3", base: "ou=People,dc=rubyldap,dc=com").first
+      end
+    end
+
+    assert_equal "user3", nested_entry.uid.first
+    assert_equal %w(user1 user2), entries
+  end
 end
