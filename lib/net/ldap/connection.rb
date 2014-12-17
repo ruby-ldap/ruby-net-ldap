13ca7c9dee5bcb9e91c8f87c482f2eba885e7787
Jerry Cheung <jch@whatcodecraves.com>
2014-12-17 09:46:31 -0800

    encryption defaults empty :tls_options

--- lib/net/ldap/connection.rb
+++ lib/net/ldap/connection.rb
@@ -89,9 +89,10 @@ class Net::LDAP::Connection #:nodoc:
   # generously contributing the :start_tls path.
   #++
   def setup_encryption(args)
+    args[:tls_options] ||= {}
     case args[:method]
     when :simple_tls
-      @conn = self.class.wrap_with_ssl(@conn, args.fetch(:tls_options, {}))
+      @conn = self.class.wrap_with_ssl(@conn, args[:tls_options])
       # additional branches requiring server validation and peer certs, etc.
       # go here.
     when :start_tls
