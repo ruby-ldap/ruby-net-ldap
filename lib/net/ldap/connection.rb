51f12c5f9f6ed600fea929ee210117045020d322
JPvRiel <jp.vanriel@gmail.com>
2016-01-20 20:30:47 +0200

    fix a regression in a prior commit and check if :verify_mode key exists otherwise a nil error results for connections without encryption

--- lib/net/ldap/connection.rb
+++ lib/net/ldap/connection.rb
@@ -51,8 +51,10 @@ class Net::LDAP::Connection #:nodoc:
     hosts.each do |host, port|
       begin
         prepare_socket(server.merge(socket: @socket_class.new(host, port, socket_opts)))
-        if encryption[:tls_options][:verify_mode] != OpenSSL::SSL::VERIFY_NONE
+        if encryption && ! encryption[:tls_options] && encryption[:tls_options][:verify_mode]
+          if encryption[:tls_options][:verify_mode] != OpenSSL::SSL::VERIFY_NONE
             @conn.post_connection_check(host)
+          end
         end
         return
       rescue Net::LDAP::Error, SocketError, SystemCallError,
