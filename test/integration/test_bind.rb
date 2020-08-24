e47848726d4ae522ac806a9864952c1743884bca
Kevin McCormack <harlemsquirrel@mccormack.tech>
2020-08-23 23:53:58 -0400

    Add GitHub CI
    Run our test suite and Rubcop in GitHub Actions.
    
    This also adds a docker-compose.yml that allows for anyone to run the CI jobs fully containerized.

--- test/integration/test_bind.rb
+++ test/integration/test_bind.rb
@@ -57,7 +57,7 @@ class TestBindIntegration < LDAPIntegrationTestCase
   end
 
   def test_bind_tls_with_bad_hostname_verify_none_no_ca_opt_merge_passes
-    @ldap.host = '127.0.0.1'
+    @ldap.host = 'cert.mismatch.example.org'
     @ldap.encryption(
       method:      :start_tls,
       tls_options: TLS_OPTS.merge(verify_mode: OpenSSL::SSL::VERIFY_NONE),
@@ -67,7 +67,7 @@ class TestBindIntegration < LDAPIntegrationTestCase
   end
 
   def test_bind_tls_with_bad_hostname_verify_peer_ca_fails
-    @ldap.host = '127.0.0.1'
+    @ldap.host = 'cert.mismatch.example.org'
     @ldap.encryption(
       method:      :start_tls,
       tls_options: { verify_mode: OpenSSL::SSL::VERIFY_PEER,
@@ -84,7 +84,7 @@ class TestBindIntegration < LDAPIntegrationTestCase
   end
 
   def test_bind_tls_with_bad_hostname_ca_default_opt_merge_fails
-    @ldap.host = '127.0.0.1'
+    @ldap.host = 'cert.mismatch.example.org'
     @ldap.encryption(
       method:      :start_tls,
       tls_options: TLS_OPTS.merge(ca_file: CA_FILE),
@@ -100,7 +100,7 @@ class TestBindIntegration < LDAPIntegrationTestCase
   end
 
   def test_bind_tls_with_bad_hostname_ca_no_opt_merge_fails
-    @ldap.host = '127.0.0.1'
+    @ldap.host = 'cert.mismatch.example.org'
     @ldap.encryption(
       method:      :start_tls,
       tls_options: { ca_file: CA_FILE },
@@ -138,7 +138,7 @@ class TestBindIntegration < LDAPIntegrationTestCase
   end
 
   def test_bind_tls_with_bogus_hostname_system_ca_fails
-    @ldap.host = '127.0.0.1'
+    @ldap.host = 'cert.mismatch.example.org'
     @ldap.encryption(method: :start_tls, tls_options: {})
     error = assert_raise Net::LDAP::Error,
                          Net::LDAP::ConnectionRefusedError do
@@ -164,7 +164,7 @@ class TestBindIntegration < LDAPIntegrationTestCase
 
   def test_bind_tls_with_multiple_bogus_hosts
     @ldap.host = nil
-    @ldap.hosts = [['127.0.0.1', 389], ['bogus.example.com', 389]]
+    @ldap.hosts = [['cert.mismatch.example.org', 389], ['bogus.example.com', 389]]
     @ldap.encryption(
       method:      :start_tls,
       tls_options: TLS_OPTS.merge(verify_mode: OpenSSL::SSL::VERIFY_PEER,
@@ -180,7 +180,7 @@ class TestBindIntegration < LDAPIntegrationTestCase
 
   def test_bind_tls_with_multiple_bogus_hosts_no_verification
     @ldap.host = nil
-    @ldap.hosts = [['127.0.0.1', 389], ['bogus.example.com', 389]]
+    @ldap.hosts = [['cert.mismatch.example.org', 389], ['bogus.example.com', 389]]
     @ldap.encryption(
       method:      :start_tls,
       tls_options: TLS_OPTS.merge(verify_mode: OpenSSL::SSL::VERIFY_NONE),
@@ -191,7 +191,7 @@ class TestBindIntegration < LDAPIntegrationTestCase
 
   def test_bind_tls_with_multiple_bogus_hosts_ca_check_only_fails
     @ldap.host = nil
-    @ldap.hosts = [['127.0.0.1', 389], ['bogus.example.com', 389]]
+    @ldap.hosts = [['cert.mismatch.example.org', 389], ['bogus.example.com', 389]]
     @ldap.encryption(
       method: :start_tls,
       tls_options: { ca_file: CA_FILE },
